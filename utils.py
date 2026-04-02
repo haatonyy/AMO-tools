28a5dfe188951444b4effefe06d1fd901efcf4bb
aqua <aqua@localhost.localdomain>
2026-04-02 17:14:44 -0500

    updates on 04/02/2026

---
+++ utils.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def get_data(file:str, skiprows=18, oscilloscope_model=None):
+    #oscilloscope saves data as csv file
+    data = np.loadtxt(file, delimiter=',', skiprows=skiprows, usecols= [3,4])
+    return data[:, 0], data[:,1]
